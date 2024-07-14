486050cd34433231c26a571c49f7aab1c56915b6
rulerpe <neetisthebest@gmail.com>
2024-07-14 14:01:19 -0700

    remove github deplyment related file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/japan-food-map-frontend",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
